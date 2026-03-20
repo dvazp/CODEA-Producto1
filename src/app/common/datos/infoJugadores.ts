7b23f6ff371f566b460a75ac90113561be95e5fe
Davivp23 <davidvazpe23@gmail.com>
2026-03-20 12:48:57 +0100

    Convertido playersComponent a bootstrap y formateado según figma

--- src/app/common/datos/infoJugadores.ts
+++ src/app/common/datos/infoJugadores.ts
@@ -4,34 +4,34 @@ export const infoJugadores = [
         posicion: "Escolta", altura: "1.98m", 
         edad: 41, pPP: 25.0, rPP: 5.2, aPP: 5, 
         porcentajeTiros: 44.7,
-        img: "https://cdn.shopify.com/s/files/1/0594/4639/5086/files/Kobe_Bryant_Signature_29f10199-efc4-4628-a0b8-75a58e22d6dd.webp?v=1747155011" },
+        img: "/assets/photos/_110655507_kb.jpg" },
     
         { nombre: "Michael Jordan", 
         equipo: "Chicago Bulls", 
         posicion: "Escolta", altura: "1.98m", 
         edad: 57, pPP: 30.1, rPP: 6.2, aPP: 5.3, 
         porcentajeTiros: 49.7,
-        img: "https://cdn.shopify.com/s/files/1/0594/4639/5086/files/Michael_Jordan_936e8919-626c-4a27-aaeb-94c4e9b7da0a.webp?v=1747152681" },
+        img: "/assets/photos/jordan_michael_1.jpg" },
     
         { nombre: "LeBron James", 
         equipo: "Los Angeles Lakers", 
         posicion: "Alero", altura: "2.06m", 
         edad: 36, pPP: 27.0, rPP: 7.4, aPP: 7.4, 
         porcentajeTiros: 50.4,
-        img: "https://cdn.shopify.com/s/files/1/0594/4639/5086/files/LeBron_James_Signature_f4bec6c2-eb73-453e-a4ee-e319195c9cef.webp?v=1747220561" },
+        img: "/assets/photos/lebron-james-los-angeles-lakers_8tf0xx4nad6e1t1gpm6fdczyc.png" },
     
-        { nombre: "Shaquille O'Neal", 
+        { nombre: "Kareem Abdul-Jabbar", 
         equipo: "Los Angeles Lakers", 
-        posicion: "Pívot", altura: "2.16m", 
-        edad: 48, pPP: 23.7, rPP: 10.9, aPP: 2.5, 
-        porcentajeTiros: 58.2,
-        img: "https://famousink.com/cdn/shop/products/shaq_filnals_mvp_500x_crop_center.jpg?v=1651272244" },
+        posicion: "Pívot", altura: "2.18m", 
+        edad: 73, pPP: 24.6, rPP: 11.2, aPP: 3.6, 
+        porcentajeTiros: 55.9,
+        img: "/assets/photos/kareem-abdul-jabbar-zfvtrzdmlck91kbkypla29ass-1584662719315.webp" },
 
-        { nombre: "Tony Parker",
-        equipo: "San Antonio Spurs", 
-        posicion: "Escolta", altura: "1.81m", 
-        edad: 38, pPP: 15.0, rPP: 5.0, aPP: 6.0, 
-        porcentajeTiros: 48.0,
-        img: "https://http2.mlstatic.com/D_NQ_NP_714446-MLM95512452661_102025-O.webp" }
+        { nombre: "Magic Johnson",
+        equipo: "Los Angeles Lakers",
+        posicion: "Base", altura: "2.06m",
+        edad: 62, pPP: 19.5, rPP: 7.2, aPP: 11.2,
+        porcentajeTiros: 52.0,
+        img: "/assets/photos/Magic-Johnson.webp" }
 
 ];
